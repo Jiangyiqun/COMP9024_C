091790fb8be5f51068529a6b64bbebacfa1690d4
Jack Jiang <jackjyq@outlook.com>
2017-12-28 18:55:01 +1100

    ass 1

--- ass1/assn1/listIteratorInt.c
+++ ass1/assn1/listIteratorInt.c
@@ -1,7 +1,7 @@
 /*
-  listIteratorInt.c : list Iterator ADT implementation
-  Written by .... 
-  Date: ....
+    listIteratorInt.c : list Iterator ADT implementation
+    Written by .... 
+    Date: ....
 */
 
 #include <stdlib.h>
@@ -10,250 +10,310 @@
 #include "listIteratorInt.h"
 
 typedef struct Node {
-  int value;
-  struct Node *prev;
-  struct Node *next;
+    int value;
+    struct Node *prev;
+    struct Node *next;
 } Node;
 
 typedef struct IteratorIntRep {
-  Node *cursor;
-  Node *first;
-  Node *last;  
-  int nitems;   // number of items, cursor is excluded
-  int lcom;     // last command, 1: next; -1: prev; 0: NULL
+    Node *cursor;
+    Node *first;
+    Node *last;    
+    int nitems;     // number of items, cursor is included
+    int lcom;         // last command, 1: next; -1: prev; 0: NULL
 } IteratorIntRep;
 
 
 // private function
 Node *newNode(int it)
 {
-  Node *new = malloc(sizeof(Node));
-  assert(new != NULL);
-  new->value = it;
-  new->prev = NULL;
-  new->next = NULL;
-  return new;
+    Node *new = malloc(sizeof(Node));
+    assert(new != NULL);
+    new->value = it;
+    new->prev = NULL;
+    new->next = NULL;
+    return new;
 }
 
 
 // ADT implementation
 IteratorInt IteratorIntNew(){
-  struct IteratorIntRep *it = malloc(sizeof(struct IteratorIntRep));
-  assert(it != NULL);
-  Node *cursorNode = newNode(0);  // the value of cursor is meaningless
-  it->cursor = cursorNode;
-  it->first = cursorNode;
-  it->last = cursorNode;
-  it->nitems = 0;
-  it->lcom = 0;  // last command, 1: next; -1: prev; 0: NULL
-  return it;
+    struct IteratorIntRep *it = malloc(sizeof(struct IteratorIntRep));
+    assert(it != NULL);
+    Node *cursorNode = newNode(0);    // the value of cursor is meaningless
+    it->cursor = cursorNode;
+    it->first = cursorNode;
+    it->last = cursorNode;
+    it->nitems = 1;  // include cursor
+    it->lcom = 0;    // last command, 1: next; -1: prev; 0: NULL
+    return it;
 }
 
 void reset(IteratorInt it){
-
-  // implement this function 
-
+    assert(it != NULL);
+    if (it->cursor != it->first ) {
+        // cursor is not the first, so before must exists
+        // oldfirst ... before ^ after
+        // ^ oldfirst ... before after
+        Node *oldfirst = it->first;         //  can be before
+        Node *before = it->cursor->prev;
+        Node *after = it->cursor->next;     // can be Null
+        it->cursor->prev = NULL;
+        it->cursor->next = oldfirst;
+        oldfirst->prev = it->cursor;
+        before->next = after;
+        if (after != NULL) {
+            after->prev = before;
+        }
+        // generate new first and last
+        it->first = it->cursor;
+        if (it->last == it->cursor) {    
+            it->last = before;
+        }
+    }
+    // else if cursor is already the first, then nothing need to be done
+    it->lcom = 0;
 }
 
 
 int add(IteratorInt it, int v){
-  assert(it != NULL);
-  Node *new = newNode(v);
-  if (it->cursor == it->first) {
-    // ^
-    // new(become first) ^
-    new->next = it->cursor;
-    it->cursor->prev = new;
-    it->first = new;
-  } else {
-    // before ^
-    // before new ^
-    Node *before = it->cursor->prev;
-    before->next = new;
-    new->prev = before;
-    new->next = it->cursor;
-    it->cursor->prev = new;
-  }
-  it->nitems++;
-  it->lcom = 0;
-  return 1;
+    assert(it != NULL);
+    Node *new = newNode(v);
+    if (it->cursor == it->first) {
+        // ^
+        // new(become first) ^
+        new->next = it->cursor;
+        it->cursor->prev = new;
+        it->first = new;
+    } else {
+        // before ^
+        // before new ^
+        Node *before = it->cursor->prev;
+        before->next = new;
+        new->prev = before;
+        new->next = it->cursor;
+        it->cursor->prev = new;
+    }
+    it->nitems++;
+    it->lcom = 0;
+    return 1;
 }
 
 
 int hasNext(IteratorInt it){
-  
-  // implement this function 
-  
-  return 0;  // you need to change this...
+        assert(it != NULL);
+        it->lcom = 0;
+        return (it->cursor != it->last);
 }
 
 int hasPrevious(IteratorInt it){
-  
-  // implement this function 
-  
-  return 0;  // you need to change this...
+        assert(it != NULL);
+        it->lcom = 0;
+        return (it->cursor != it->first);
 }
 
 
 int *next(IteratorInt it){
-  assert(it != NULL);
-  if (it->cursor == it->last) {
-    // sth ^
-    return NULL;
-  } else if (it->cursor == it->first) {
-    // ^ before after
-    // before(become the first) ^ after
-    Node *before = it->cursor->next;
-    Node *after = before->next;
-    it->first = before;
-    before->next = it->cursor;
-    it->cursor->prev = before;
-    it->cursor->next = after;
-    after->prev = it->cursor;
-    if (after->next == NULL) {
-      it->last = after;
+    assert(it != NULL);
+    if (it->cursor == it->last) {
+        // cursor is the last element, then there will be no next
+        // ^ (last)
+        // no next element
+        it->lcom = 0;
+        return NULL;
+    } else {
+        // cursor is not the last element, then before must exists
+        // bbefore ^ before after
+        // bbefore before ^ after
+        Node *before = it->cursor->next;
+        Node *after = before->next;     // can be NULL
+        Node *bbefore = it->cursor->prev;     // can be NULL
+        if (bbefore != NULL) {
+            bbefore->next = before;
+        }
+        before->prev = bbefore;
+        before->next = it->cursor;
+        it->cursor->prev = before;
+        it->cursor->next = after;
+        if (after != NULL) {
+            after->prev = it->cursor;
+        }
+        
+        // generate new first and last
+        if (bbefore == NULL) {
+            it->first = before;
+        } else if (bbefore->prev == NULL) {
+            it->first = bbefore;
+        }
+        if (after == NULL) {
+            it->last = it->cursor;
+        } else if (after->next == NULL) {
+            it->last = after;
+        }
+        it->lcom = 1;
+        return &(before->value);
     }
-    it->lcom = 1;
-    return &(before->value);
-  } else {
-    // bbefore ^ before after
-    // bbefore before ^ after
-    Node *bbefore = it->cursor->prev;
-    Node *before = it->cursor->next;
-    Node *after = before->next;
-    bbefore->next = before;
-    before->prev = bbefore;
-    before->next = it->cursor;
-    it->cursor->prev = before;
-    it->cursor->next = after;
-    after->prev = it->cursor;
-    if (after->next == NULL) {
-      it->last = after;
-    }
-    it->lcom = 1;
-    return &(before->value);
-  }
 }
 
 int *previous(IteratorInt it){
-  assert(it != NULL);
-  if (it->cursor == it->first) {
-    return NULL;
-  } else if (it->cursor == it->last) {
-    // before after ^
-    // before ^ after(become the last)
-    Node *after = it->cursor->prev;
-    Node *before = after->prev;
-    before->next = it->cursor;
-    it->cursor->prev = before;
-    it->cursor->next = after;
-    after->prev = it->cursor;
-    it->last = after;
-    if (before->prev == NULL) {
-      it->first = before;
+    assert(it != NULL);
+    if (it->cursor == it->first) {
+        // cursor is the first element, then there will be no previous
+        // ^(first)
+        // no previous element
+        it->lcom = 0;
+        return NULL;
+    } else {
+        // cursor is not the first element, then after must exists
+        // before after ^ aafter
+        // before ^ after aafter
+        Node *after = it->cursor->prev;
+        Node *before = after->prev;     // can be NULL
+        Node *aafter = it->cursor->next;    // can be NULL
+        if (before != NULL) {
+            before->next = it->cursor;
+        }
+        it->cursor->prev = before;
+        it->cursor->next = after;
+        after->prev = it->cursor;
+        after->next = aafter;
+        if (aafter != NULL) {
+            aafter->prev = after;
+        }
+        // generate new first and last
+        if (before == NULL) {
+            it->first = it->cursor;
+        } else if (before->prev == NULL) {
+            it->first = before;
+        }
+        if (aafter == NULL) {
+            it->last = after;
+        } else if (aafter->next == NULL) {
+            it->last = aafter;
+        }
+        it->lcom = -1;
+        return &(after->value);
     }
-    it->lcom = -1;
-    return &(after->value);
-  } else {
-    // before after ^ aafter
-    // before ^ after aafter
-    Node *after = it->cursor->prev;
-    Node *before = after->prev;
-    Node *aafter = it->cursor->next;
-    before->next = it->cursor;
-    it->cursor->prev = before;
-    it->cursor->next = after;
-    after->prev = it->cursor;
-    after->next = aafter;
-    aafter->prev = after;
-    if (before->prev == NULL) {
-      it->first = before;
-    }
-    it->lcom = -1;
-    return &(after->value);
-  }
 }
 
 
 
 int deleteElm(IteratorInt it){
-  assert(it != NULL);
-  if (it->lcom == 0) {
-    return 0;
-  } else if (it->lcom == 1) {
-    // last command is next, then we need to delete before
-    Node *before = it->cursor->prev;
-    Node *after = it->cursor->next;
-    if (before == it->first) {
-      // before ^ after
-      // ^(become the first) after
-      it->first = it->cursor;
-      it->cursor->next = after;
-      after->prev = it->cursor;
-      free(before);
-    } else {
-      // bbefore before ^ after
-      // bbefore ^ after
-      Node *bbefore = before->prev;
-      bbefore->next = it->cursor;
-      it->cursor->prev = bbefore;
-      it->cursor->next = after;
-      after->prev = it->cursor;
-      free(before);
+    assert(it != NULL);
+    if (it->lcom == 0) {
+        return 0;
+    } else if (it->lcom == 1) {
+        // last command is next, then we need to delete before
+        Node *before = it->cursor->prev;
+        Node *after = it->cursor->next;
+        if (before == it->first) {
+            // before ^ after
+            // ^(become the first) after
+            it->first = it->cursor;
+            it->cursor->next = after;
+            after->prev = it->cursor;
+            free(before);
+        } else {
+            // bbefore before ^ after
+            // bbefore ^ after
+            Node *bbefore = before->prev;
+            bbefore->next = it->cursor;
+            it->cursor->prev = bbefore;
+            it->cursor->next = after;
+            after->prev = it->cursor;
+            free(before);
+        }
+    } else if (it->lcom == -1) {
+        // last command is prev, then we need to delete after
+        Node *before = it->cursor->prev;
+        Node *after = it->cursor->next;
+        if (after == it->last) {
+            // before ^ after
+            // before ^(become the last)
+            before->next = it->cursor;
+            it->cursor->prev = before;
+            it->last = it->cursor;
+            free(after);
+        } else {
+            // before ^ after aafter
+            // before ^ aafter
+            Node *aafter = after->next;
+            before->next = it->cursor;
+            it->cursor->prev = before;
+            it->cursor->next = aafter;
+            aafter->prev = it->cursor;
+            free(after);
+        }
     }
-  } else if (it->lcom == -1) {
-    // last command is prev, then we need to delete after
-    Node *before = it->cursor->prev;
-    Node *after = it->cursor->next;
-    if (after == it->last) {
-      // before ^ after
-      // before ^(become the last)
-      before->next = it->cursor;
-      it->cursor->prev = before;
-      it->last = it->cursor;
-      free(after);
-    } else {
-      // before ^ after aafter
-      // before ^ aafter
-      Node *aafter = after->next;
-      before->next = it->cursor;
-      it->cursor->prev = before;
-      it->cursor->next = aafter;
-      aafter->prev = it->cursor;
-      free(after);
-    }
-  }
-  return 1;
+    it->nitems--;
+    it->lcom = 0;
+    return 1;
 }
 
 
 int set(IteratorInt it, int v){
-  
-  // implement this function 
-
-  return 0;
+    assert(it != NULL);
+    if (it->lcom == 0) {
+        return 0;
+    } else if (it->lcom == 1) {
+        // last command is next, then before must exists
+        // before ^
+        it->cursor->prev->value = v;
+    } else if (it->lcom == -1) {
+        it->cursor->next->value = v;
+    }
+    it->lcom = 0;
+    return 1;
 }
 
 int *findNext(IteratorInt it, int v){
-
-  // implement this function 
-  
-  return 0;
+    assert(it != NULL);
+    int nmove = 0; // number of moves
+    while (it->cursor != it->last) {
+        int *n = next(it);
+        if (*n == v) {
+            it->lcom = 1;
+            return n;
+        }
+        nmove++;
+    }
+    for (; nmove > 0; nmove-- ) {
+        previous(it);
+    }
+    it->lcom = 0;
+    return NULL;
 }
 
 int *findPrevious(IteratorInt it, int v){
-  
-  // implement this function 
-  
-  return 0;
+    assert(it != NULL);
+    int nmove = 0; // number of moves
+    while (it->cursor != it->first) {
+        int *n = previous(it);
+        if (*n == v) {
+            it->lcom = -1;
+            return n;
+        }
+        nmove++;
+    }
+    for (; nmove > 0; nmove-- ) {
+        next(it);
+    }
+    it->lcom = 0;
+    return NULL;
 }
 
 void freeIt(IteratorInt it){
-
-  // implement this function 
-  
-  return;
-
+    assert(it != NULL);
+    // A        B       C        D       E...
+    //FreeNow  FreeNext
+    Node *freeNow = it->first;
+    Node *freeNext;
+    for (; it->nitems > 0; it->nitems--) {
+        freeNext = freeNow->next;
+        free(freeNow);
+        freeNow = freeNext;
+    }
+    free(it);
 }
 
 
--- ass1/assn1/testListIteratorInt.c
+++ ass1/assn1/testListIteratorInt.c
@@ -35,62 +35,187 @@ void prevdsp(IteratorInt it){
 void deletedsp(IteratorInt it){
   printf("delete, returns %d \n", deleteElm(it)) ;   
 }
-
+void hasnextdsp(IteratorInt it){
+  printf("hasnext, returns %d \n", hasNext(it) ) ;   
+}
+void hasprevdsp(IteratorInt it){
+  printf("hasprev, returns %d \n", hasPrevious(it) ) ;   
+}
+void resetdsp(IteratorInt it){
+  reset(it);
+  printf("reset\n") ;   
+}
+void findnextdsp(IteratorInt it, int v){
+  printf("findnext %d, returns %s \n", v, myitoa(findNext(it, v)) ) ;   
+}
+void findprevdsp(IteratorInt it, int v){
+  printf("findprev %d, returns %s \n", v, myitoa(findPrevious(it, v)) ) ;   
+}
+void setdsp(IteratorInt it, int v){
+  printf("set %d, returns %d \n", v, set(it, v)) ;   
+}
+void freedsp(IteratorInt it){
+  freeIt(it);
+  printf("free\n") ;   
+}
 /*  You may need to add other Helper functions ! 
     ------ End of Helper functions for Operations on ADT -------
 */
 
 int main(int argc, char *argv[]) {
   
-	  /* The following code, may or may not be correct!
-	     It's provided in case you find it difficult to understand 
-	     how to test your list iterator.
-	     The code below only tests basic operations, however 
-	     make sure that you do test all the required functions properly in
-	     your test file. 
-	  */
-	  
-	  IteratorInt it = IteratorIntNew();
-	  int TestNo = 0;
+    /* The following code, may or may not be correct!
+       It's provided in case you find it difficult to understand 
+       how to test your list iterator.
+       The code below only tests basic operations, however 
+       make sure that you do test all the required functions properly in
+       your test file. 
+    */
+    
+    IteratorInt it = IteratorIntNew();
+    int TestNo = 0;
 
-	  assert(argc > 1);
-	  sscanf(argv[1], "%d", &TestNo);
-	  
-	  if(TestNo == 1) { 
-			adddsp(it, 25);
-			adddsp(it, 14);
-			adddsp(it, 32);
-			adddsp(it, 53);
-			adddsp(it, 8);
-	    prevdsp(it);
-	    prevdsp(it);
-	    prevdsp(it);
-	  } else if(TestNo == 2) {
-			adddsp(it, 25);
-			adddsp(it, 14);
-			adddsp(it, 32);
-			adddsp(it, 53);
-			adddsp(it, 8);
-	    prevdsp(it);
-	    prevdsp(it);
-	    prevdsp(it);   
-	    nextdsp(it);
-	    nextdsp(it);
-	  } else if(TestNo == 3) { 
-			adddsp(it, 25);
-			adddsp(it, 14);
-			adddsp(it, 32);
-			adddsp(it, 53);
-			adddsp(it, 8);
-	    prevdsp(it);
-	    prevdsp(it);
-	    prevdsp(it);
-	    deletedsp(it);
-	    nextdsp(it);
-	    prevdsp(it);
-	    prevdsp(it);
-	  }
+    assert(argc > 1);
+    sscanf(argv[1], "%d", &TestNo);
+    
+    if(TestNo == 1) { 
+      adddsp(it, 25);
+      adddsp(it, 14);
+      adddsp(it, 32);
+      adddsp(it, 53);
+      adddsp(it, 8);
+      prevdsp(it);
+      prevdsp(it);
+      prevdsp(it);
+    } else if(TestNo == 2) {
+      adddsp(it, 25);
+      adddsp(it, 14);
+      adddsp(it, 32);
+      adddsp(it, 53);
+      adddsp(it, 8);
+      prevdsp(it);
+      prevdsp(it);
+      prevdsp(it);   
+      nextdsp(it);
+      nextdsp(it);
+    } else if(TestNo == 3) { 
+      adddsp(it, 25);
+      adddsp(it, 14);
+      adddsp(it, 32);
+      adddsp(it, 53);
+      adddsp(it, 8);
+      prevdsp(it);
+      prevdsp(it);
+      prevdsp(it);
+      deletedsp(it);
+      nextdsp(it);
+      prevdsp(it);
+      prevdsp(it);
+    } else if (TestNo == 4) {
+      adddsp(it, 25);
+      adddsp(it, 14);
+      adddsp(it, 32);
+      nextdsp(it);
+      nextdsp(it);
+      prevdsp(it);
+      prevdsp(it);
+      prevdsp(it);
+      prevdsp(it);
+      nextdsp(it);
+      nextdsp(it);
+      nextdsp(it);
+      nextdsp(it);
+    } else if (TestNo == 5) {
+      nextdsp(it);
+      prevdsp(it);
+      deletedsp(it);
+      deletedsp(it);
+    } else if (TestNo == 6) {
+      adddsp(it, 25);
+      adddsp(it, 14);
+      adddsp(it, 32);
+      adddsp(it, 53);
+      adddsp(it, 8);
+      deletedsp(it);
+      deletedsp(it);
+      prevdsp(it);
+      prevdsp(it);
+      deletedsp(it);
+      nextdsp(it);
+      nextdsp(it);
+      deletedsp(it);
+    } else if (TestNo == 7) {
+      adddsp(it, 25);
+      adddsp(it, 14);
+      adddsp(it, 32);
+      hasnextdsp(it);
+      hasprevdsp(it);
+      prevdsp(it);
+      prevdsp(it);
+      hasnextdsp(it);
+      hasprevdsp(it);
+      prevdsp(it);
+      hasnextdsp(it);
+      hasprevdsp(it);
+    } else if (TestNo == 8) {
+      adddsp(it, 25);
+      adddsp(it, 14);
+      adddsp(it, 32);
+      resetdsp(it);
+      hasprevdsp(it);
+      nextdsp(it);
+      nextdsp(it);
+    } else if (TestNo == 9) {
+      adddsp(it, 25);
+      adddsp(it, 14);
+      adddsp(it, 32);
+      adddsp(it, 53);
+      adddsp(it, 8);
+      findnextdsp(it, 0);
+      prevdsp(it);
+      prevdsp(it);
+      prevdsp(it);
+      findnextdsp(it, 666);
+      prevdsp(it);
+      nextdsp(it);
+      findnextdsp(it, 53);
+      nextdsp(it);
+      prevdsp(it);
+      prevdsp(it);
+    } else if (TestNo == 10) {
+      adddsp(it, 25);
+      adddsp(it, 14);
+      adddsp(it, 32);
+      adddsp(it, 53);
+      adddsp(it, 8);
+      findprevdsp(it, 14);
+      prevdsp(it);
+      prevdsp(it);
+      findprevdsp(it, 32);
+      nextdsp(it);
+    } else if (TestNo == 11) {
+      adddsp(it, 25);
+      adddsp(it, 14);
+      adddsp(it, 32);
+      adddsp(it, 53);
+      adddsp(it, 8);
+      setdsp(it, 6);
+      prevdsp(it);
+      prevdsp(it);
+      setdsp(it, -53);
+      nextdsp(it);
+      setdsp(it, 62);
+      prevdsp(it);
+      prevdsp(it);
+    } else if (TestNo == 12) {
+      adddsp(it, 25);
+      adddsp(it, 14);
+      adddsp(it, 32);
+      adddsp(it, 53);
+      adddsp(it, 8);
+      freedsp(it);
+    }
 
 
-  	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
